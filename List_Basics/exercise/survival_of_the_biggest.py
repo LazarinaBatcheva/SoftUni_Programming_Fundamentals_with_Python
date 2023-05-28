c020e26970ed42b27054461b8c993ce45b1b481c
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-05-28 16:41:54 +0200

    Update survival_of_the_biggest.py

--- List_Basics/exercise/survival_of_the_biggest.py
+++ List_Basics/exercise/survival_of_the_biggest.py
@@ -2,16 +2,16 @@ list_of_integers = input().split()
 count_of_numbers_to_remove = int(input())
 
 remaining_numbers = []
-numbers_counter = 0
+removed_numbers_counter = 0
 
 for element in list_of_integers:
     current_number = int(element)
     remaining_numbers.append(current_number)
 
-while numbers_counter < count_of_numbers_to_remove:
+while removed_numbers_counter < count_of_numbers_to_remove:
     min_number = min(remaining_numbers)
     remaining_numbers.remove(min_number)
-    numbers_counter += 1
+    removed_numbers_counter += 1
 
 list_of_integers = remaining_numbers
 
