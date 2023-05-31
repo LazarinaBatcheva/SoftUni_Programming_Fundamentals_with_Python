cbd21af746fbec54a93868ab13286ecbdbdefdeb
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-05-31 13:37:27 +0200

    Update zeros_to_back.py

--- List_Basics/more_exercises/zeros_to_back.py
+++ List_Basics/more_exercises/zeros_to_back.py
@@ -1,16 +1,16 @@
 numbers_as_string = input().split(', ')
 
-zeroes_counter = 0
+zeros_counter = 0
 list_of_numbers = []
 
 for element in numbers_as_string:
     current_number = int(element)
     if current_number == 0:
-        zeroes_counter += 1
+        zeros_counter += 1
     else:
         list_of_numbers.append(current_number)
 
-for zeroes in range(zeroes_counter):
+for zeros in range(zeros_counter):
     list_of_numbers.append(0)
 
 print(list_of_numbers)
