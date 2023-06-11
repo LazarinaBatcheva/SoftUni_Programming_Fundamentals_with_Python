c0a0af327913338458389332fd2b8dc6622048f8
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-06-11 16:26:49 +0200

    Update array_modifier.py

--- mid_exam_exercises/array_modifier.py
+++ mid_exam_exercises/array_modifier.py
@@ -3,12 +3,15 @@ command = input()
 
 while command != "end":
     current_command = command.split()
+    
     if current_command[0] == "swap":
         index_1, index_2 = int(current_command[1]), int(current_command[2])
         elements[index_1], elements[index_2] = elements[index_2], elements[index_1]
+        
     elif current_command[0] == "multiply":
         index_1, index_2 = int(current_command[1]), int(current_command[2])
         elements[index_1] *= elements[index_2]
+        
     elif current_command[0] == "decrease":
         elements = [element - 1 for element in elements]
 
