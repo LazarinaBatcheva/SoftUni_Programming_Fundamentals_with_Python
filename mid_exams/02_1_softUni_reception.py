29b36e8227e304f8d1432f22f468587b5b46160c
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-06-13 11:19:24 +0200

    Update 02_1_softUni_reception.py

--- mid_exams/02_1_softUni_reception.py
+++ mid_exams/02_1_softUni_reception.py
@@ -2,18 +2,34 @@ first_employee, second_employee, third_employee = int(input()), int(input()), in
 students = int(input())
 
 answers_per_hour = first_employee + second_employee + third_employee
-
-working_hours = 0  # work without break - 3 hours
-time_needed_to_answers = 0
+time_needed = 0
 
 while students > 0:
-    if working_hours == 3:
-        time_needed_to_answers += 1
-        working_hours = 0
-        continue
-
-    working_hours += 1
-    time_needed_to_answers += 1
+    if students > 0:
+        time_needed += 1
+        if time_needed % 4 == 0:
+            time_needed += 1
     students -= answers_per_hour
 
-print(f"Time needed: {time_needed_to_answers}h.")
+print(f"Time needed: {time_needed}h.")
+
+
+# first_employee, second_employee, third_employee = int(input()), int(input()), int(input())
+# students = int(input())
+
+# answers_per_hour = first_employee + second_employee + third_employee
+
+# working_hours = 0  # work without break - 3 hours
+# time_needed_to_answers = 0
+
+# while students > 0:
+#     if working_hours == 3:
+#         time_needed_to_answers += 1
+#         working_hours = 0
+#         continue
+
+#     working_hours += 1
+#     time_needed_to_answers += 1
+#     students -= answers_per_hour
+
+# print(f"Time needed: {time_needed_to_answers}h.")
