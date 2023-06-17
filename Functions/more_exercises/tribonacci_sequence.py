43be79ed6b3e1ab24f889cf4f8ae4d19a63f80aa
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-06-17 15:52:33 +0200

    Update tribonacci_sequence.py

--- Functions/more_exercises/tribonacci_sequence.py
+++ Functions/more_exercises/tribonacci_sequence.py
@@ -4,7 +4,7 @@ def tribonacci_sequence(some_number):
         new_number = sum(numbers_list)
         print(new_number, end=" ")
         numbers_list.append(new_number)
-        numbers_list.pop(0)
+        numbers_list.remove(numbers_list[0])
 
 
 number = int(input())
