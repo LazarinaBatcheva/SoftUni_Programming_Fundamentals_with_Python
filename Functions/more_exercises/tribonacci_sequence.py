59417219e70cfa76fd74920686bfc93d842e432d
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-06-17 15:48:18 +0200

    Add files via upload

---
+++ Functions/more_exercises/tribonacci_sequence.py
@@ -0,0 +1,12 @@
+def tribonacci_sequence(some_number):
+    numbers_list = [1, 0, 0]
+    for current_number in range(some_number):
+        new_number = sum(numbers_list)
+        print(new_number, end=" ")
+        numbers_list.append(new_number)
+        numbers_list.pop(0)
+
+
+number = int(input())
+
+tribonacci_sequence(number)
