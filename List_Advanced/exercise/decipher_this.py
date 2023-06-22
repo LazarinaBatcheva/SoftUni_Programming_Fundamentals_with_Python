5149befd9f557d37e43228e3dca8e75967875fa1
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-06-22 11:17:55 +0200

    Add files via upload

---
+++ List_Advanced/exercise/decipher_this.py
@@ -0,0 +1,23 @@
+def deciphered_message(some_message):
+    words, numbers = [], []
+    number, letter = "", ""
+    for character in some_message:
+        if character.isdigit():
+            number += character
+        else:
+            letter += character
+    numbers.append(int(number))
+    first_letter = chr(numbers[0])
+
+    if len(letter) > 0:
+        current_letter = [let for let in letter]
+        current_letter[0], current_letter[-1] = current_letter[-1], current_letter[0]
+        words.append(current_letter)
+        return first_letter + "".join(current_letter)
+
+
+message = input().split()
+
+for i in range(len(message)):
+    message[i] = deciphered_message(str(message[i]))
+print(*message)
