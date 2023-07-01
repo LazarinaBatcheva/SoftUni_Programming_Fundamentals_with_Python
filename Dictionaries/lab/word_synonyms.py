1f1c269629f94997c6b22e0aaa8d17073a076c2d
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-07-01 22:04:23 +0200

    Add files via upload

---
+++ Dictionaries/lab/word_synonyms.py
@@ -0,0 +1,12 @@
+count_of_words = int(input())
+synonyms = {}
+
+for i in range(count_of_words):
+    word = input()
+    synonym = input()
+    if word not in synonyms:
+        synonyms[word] = []
+    synonyms[word].append(synonym)
+
+for word in synonyms:
+    print(f"{word} - {', '.join(synonyms[word])}")
