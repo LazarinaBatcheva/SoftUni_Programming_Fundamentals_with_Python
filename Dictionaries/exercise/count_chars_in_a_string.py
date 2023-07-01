0a641780f7a0cdf1b444438f1097d6552337e8d9
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-07-01 22:17:37 +0200

    Update count_chars_in_a_string.py

--- Dictionaries/exercise/count_chars_in_a_string.py
+++ Dictionaries/exercise/count_chars_in_a_string.py
@@ -2,7 +2,7 @@ text = [ch for ch in input() if ch != " "]
 occurrences = {}
 
 for ch in text:
-    if ch not in occurrences:
+    if ch not in occurrences:    # if ch not in occurrences.keys():
         occurrences[ch] = 0
     occurrences[ch] += 1
 
