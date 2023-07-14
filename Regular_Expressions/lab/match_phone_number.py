528cfe22e71655877cae1401bf31d577cb3bec08
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-07-14 19:05:19 +0200

    Update match_phone_number.py

--- Regular_Expressions/lab/match_phone_number.py
+++ Regular_Expressions/lab/match_phone_number.py
@@ -1,6 +1,6 @@
 import re
 
 phone_numbers = input()
-regex = "\\+359-2-\d{3}-\d{4}\\b|\\+359 2 \d{3} \d{4}\\b"   # "\\+359-2-\\d{3}-\\d{4}\\b|\\+359 2 \\d{3} \\d{4}\\b"
+regex = "\\+359-2-\\d{3}-\\d{4}\\b|\\+359 2 \\d{3} \\d{4}\\b"
 matches = re.findall(regex, phone_numbers)
 print(", ".join(matches))
