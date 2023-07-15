b624d6b77154944543b65928efcb30f6e0285720
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-07-15 14:06:25 +0200

    Add files via upload

---
+++ Regular_Expressions/exercise/furniture.py
@@ -0,0 +1,19 @@
+import re
+
+command = input()
+pattern = r"\>>([A-Za-z]+)<<([\d]+[\.[0-9]*)!(\d+)\b"
+total_money = 0
+bought_furniture = []
+
+while command != "Purchase":
+    matches = re.search(pattern, command)
+    if matches:
+        furniture, price, quantity = matches.groups()
+        bought_furniture.append(furniture)
+        total_money += float(price) * int(quantity)
+    command = input()
+
+print("Bought furniture:")
+for current_furniture in bought_furniture:
+    print(current_furniture)
+print(f"Total money spend: {total_money:.2f}")
