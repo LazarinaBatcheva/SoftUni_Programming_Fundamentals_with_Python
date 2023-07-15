5b193995c0766b75f6648e5220b26f04587f5915
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-07-15 14:26:24 +0200

    Update extract_the_links.py

--- Regular_Expressions/exercise/extract_the_links.py
+++ Regular_Expressions/exercise/extract_the_links.py
@@ -4,9 +4,9 @@ sentence = input()
 valid_links = []
 pattern = r"(w{3}.[a-zA-Z0-9-\.]+\.[a-z]+)"
 while sentence:
-    matches = re.search(pattern, sentence)
+    matches = re.search(pattern, sentence)    # matches = re.findall(pattern, sentence)
     if matches:
-        valid_links.append(matches.group(1))
+        valid_links.append(matches.group(1))    # valid_links.append(matches[0])
     sentence = input()
 for link in valid_links:
     print(link)
