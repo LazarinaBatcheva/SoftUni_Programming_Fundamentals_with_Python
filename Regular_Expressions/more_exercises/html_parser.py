ed7bb35f281619a84ae1b21dba4b890a9406ffeb
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-07-16 21:13:12 +0200

    Update html_parser.py

--- Regular_Expressions/more_exercises/html_parser.py
+++ Regular_Expressions/more_exercises/html_parser.py
@@ -2,11 +2,11 @@ import re
 
 
 def extract_title_and_content(html_string):
-    title_pattern = r"<title>(.*?)<\/title>"
+    title_pattern = r"<title>(.*?)</title>"
     title_match = re.search(title_pattern, html_string)
     title = title_match.group(1) if title_match else ""
 
-    body_pattern = r"<body>(.*?)<\/body>"
+    body_pattern = r"<body>(.*?)</body>"
     body_match = re.search(body_pattern, html_string)
     content = body_match.group(1) if body_match else ""
     clean_content = re.sub(r"<[^<]+>", "", content).replace("\\n", "")
