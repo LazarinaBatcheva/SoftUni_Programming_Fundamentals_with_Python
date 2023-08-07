610198c4940cf7317e44360cf7444b02978a367e
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-08-07 19:06:59 +0200

    Add files via upload

---
+++ Open_Courses/Python_Fundamentals_Exercises/Functions_and_Debugging/math_power.py
@@ -0,0 +1,7 @@
+def calculate_result(first_num, second_num):
+    print(f"{first_num**second_num}")
+
+
+first_number = float(input())
+second_number = float(input())
+calculate_result(first_number, second_number)
