e5ad8f3404b2c81f0ed05007a5697ca09442a7ea
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-08-07 19:28:12 +0200

    Add files via upload

---
+++ Open_Courses/Python_Fundamentals_Exercises/Functions_and_Debugging/greater_of_two_values.py
@@ -0,0 +1,36 @@
+def found_bigger_int(first, second):
+    first, second = int(first), int(second)
+    if first > second:
+        return first
+    else:
+        return second
+
+
+def found_bigger_char(first, second):
+    if ord(first) > ord(second):
+        return first
+    else:
+        return second
+
+
+def found_bigger_string(first, second):
+    if first > second:
+        return first
+    else:
+        return second
+
+
+def base_function(v_type):
+    first = input()
+    second = input()
+    if v_type == "int":
+        result = found_bigger_int(first, second)
+    elif v_type == "char":
+        result = found_bigger_char(first, second)
+    else:
+        result = found_bigger_string(first, second)
+    return result
+
+
+values_type = input()
+print(base_function(values_type))
