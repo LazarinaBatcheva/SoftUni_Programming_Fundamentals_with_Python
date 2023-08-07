c1d8620d871533c898008e9b009dacd6be9c821b
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-08-07 19:31:59 +0200

    Update greater_of_two_values.py

--- Open_Courses/Python_Fundamentals_Exercises/Functions_and_Debugging/greater_of_two_values.py
+++ Open_Courses/Python_Fundamentals_Exercises/Functions_and_Debugging/greater_of_two_values.py
@@ -1,19 +1,4 @@
-def found_bigger_int(first, second):
-    first, second = int(first), int(second)
-    if first > second:
-        return first
-    else:
-        return second
-
-
-def found_bigger_char(first, second):
-    if ord(first) > ord(second):
-        return first
-    else:
-        return second
-
-
-def found_bigger_string(first, second):
+def found_bigger(first, second):
     if first > second:
         return first
     else:
@@ -24,13 +9,52 @@ def base_function(v_type):
     first = input()
     second = input()
     if v_type == "int":
-        result = found_bigger_int(first, second)
+        result = found_bigger(int(first), int(second))
     elif v_type == "char":
-        result = found_bigger_char(first, second)
+        result = found_bigger(first, second)
     else:
-        result = found_bigger_string(first, second)
+        result = found_bigger(first, second)
     return result
 
 
 values_type = input()
 print(base_function(values_type))
+
+
+
+# def found_bigger_int(first, second):
+#     first, second = int(first), int(second)
+#     if first > second:
+#         return first
+#     else:
+#         return second
+
+
+# def found_bigger_char(first, second):
+#     if ord(first) > ord(second):
+#         return first
+#     else:
+#         return second
+
+
+# def found_bigger_string(first, second):
+#     if first > second:
+#         return first
+#     else:
+#         return second
+
+
+# def base_function(v_type):
+#     first = input()
+#     second = input()
+#     if v_type == "int":
+#         result = found_bigger_int(first, second)
+#     elif v_type == "char":
+#         result = found_bigger_char(first, second)
+#     else:
+#         result = found_bigger_string(first, second)
+#     return result
+
+
+# values_type = input()
+# print(base_function(values_type))
