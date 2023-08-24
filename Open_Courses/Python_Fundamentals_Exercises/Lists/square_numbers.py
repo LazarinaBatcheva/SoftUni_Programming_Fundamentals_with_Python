807a33157745d414ff5251c1fb38138442e5a86d
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-08-24 17:20:46 +0200

    Update square_numbers.py

--- Open_Courses/Python_Fundamentals_Exercises/Lists/square_numbers.py
+++ Open_Courses/Python_Fundamentals_Exercises/Lists/square_numbers.py
@@ -5,8 +5,8 @@ square_numbers_list = []
 
 for number in integers_list:
     if number > 0:
-        sgrt_number = math.sqrt(number)
-        if sgrt_number.is_integer():
+        sgr_number = math.sqrt(number)
+        if sgr_number.is_integer():
             square_numbers_list.append(number)
 
 square_numbers_list.sort(reverse=True)
