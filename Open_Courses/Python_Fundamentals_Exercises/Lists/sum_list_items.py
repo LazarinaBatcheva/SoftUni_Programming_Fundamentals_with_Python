8a8c47a387bdb692939dbd0b35df702b1bf5225c
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-08-24 15:42:36 +0200

    Delete Open_Courses/Python_Fundamentals_Exercises/Lists directory

--- Open_Courses/Python_Fundamentals_Exercises/Lists/sum_list_items.py
+++
@@ -1,8 +0,0 @@
-number_of_integers = int(input())
-numbers_list = []
-
-for i in range(number_of_integers):
-    current_number = int(input())
-    numbers_list.append(current_number)
-
-print(sum(numbers_list))
