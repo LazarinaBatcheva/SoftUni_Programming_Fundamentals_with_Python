75d00f0ba0bc07e327e30a811eec0d7e6c362c56
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-09-05 20:43:46 +0200

    Update count_numbers.py

--- Open_Courses/Python_Fundamentals_Exercises/Lists_Exercises/count_numbers.py
+++ Open_Courses/Python_Fundamentals_Exercises/Lists_Exercises/count_numbers.py
@@ -1,11 +1,19 @@
-list_of_integers = [int(num) for num in input().split()]
-numbers_dict = {}
+list_of_integers = [int(x) for x in input().split()]
+numbers = []
+for num in sorted(list_of_integers):
+    if num not in numbers:
+        print(f"{num} -> {list_of_integers.count(num)}")
+        numbers.append(num)
 
-for number in list_of_integers:
-    if number not in numbers_dict.keys():
-        numbers_dict[number] = 1
-    else:
-        numbers_dict[number] += 1
 
-for num, count in sorted(numbers_dict.items()):
-    print(f"{num} -> {count}")
+# list_of_integers = [int(num) for num in input().split()]
+# numbers_dict = {}
+
+# for number in list_of_integers:
+#     if number not in numbers_dict.keys():
+#         numbers_dict[number] = 1
+#     else:
+#         numbers_dict[number] += 1
+
+# for num, count in sorted(numbers_dict.items()):
+#     print(f"{num} -> {count}")
